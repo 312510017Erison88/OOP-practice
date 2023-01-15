7023fae63f01c059441ef7210fc50b3b72aa69bb
Erison88 <songerison.mse08@nctu.edu.tw>
2023-01-15 22:55:56 +0800

    add practice.h

--- lab1/practice.cpp
+++ lab1/practice.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include "practice.h"
 using namespace std;
 
 int main(){
+
     return 0;
 }
 
-
+int function (int function_parameter){
+    cout << "hi" << endl;
+    return 1000;
+}
---
+++ lab1/practice.h
@@ -0,0 +1,8 @@
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <stdlib.h>
+
+int function (int function_parameter);
