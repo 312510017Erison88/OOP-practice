cde33117d174c5e4adebcba4bd344f1dfb26b1e0
Erison88 <songerison.mse08@nctu.edu.tw>
2023-01-17 15:56:00 +0800

    ready to read file

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <fstream>
 #include "practice.h"
 using namespace std;
 
 int main(){
+    //declare a file name variable
+    ifstream input_file_var;
+    ofstream output_filename_var;
+
+    //open the file
+    input_file_var.open("file_path/filename");
+    output_filename_var.open("file_path/filename");
+
+
+
     function(1000);
     return 0;
 }
