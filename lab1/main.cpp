26a3c726197b8ac218711a1f7fd9cef6324e2a89
Erison88 <songerison.mse08@nctu.edu.tw>
2023-01-17 16:59:12 +0800

    get start to do the Lab1

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,21 +1,41 @@
 #include <iostream>
 #include <fstream>
 #include "practice.h"
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-int main(){
+int main(int argc, char **argv){
     //declare a file name variable
-    ifstream input;
-    ofstream output;
+    ifstream input_file;
+    ofstream output_file;
 
     //open the file
-    input.open("input1.in");
-    cout << input.is_open() << endl; 
-    input.close();
+    input_file.open(argv[1], ios::in);
+    cout << input_file.is_open() << endl;
+    
+    string str;
+    vector<string> input1;
+    vector<int> input2;
+    char line[128];
+
+    while(input_file.getline(line, sizeof(line), '\n')){
+        cout<<line<<endl;
+    }
+
+/*
+    while(input_file>>str){
+        cout << str << endl;
+    } 
+*/
+    if(input_file.eof())
+        cout << "EOF" << endl;
+    else
+        cout << "error" << endl; 
+
+    input_file.close();
 
     //output.open("file_path/filename");
-
-
 
     function(1000);
     return 0;
--- lab1/practice.cpp
+++ lab1/practice.cpp
@@ -3,5 +3,5 @@
 using namespace std;
 
 void function (int function_parameter){
-    cout << "hi" << endl << function_parameter << endl;
+    cout << function_parameter << endl;
 }
