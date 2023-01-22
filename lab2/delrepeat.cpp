ad5784d93c41bf8b12625dacc16dcec340c6f81a
Erison88 <songerison.mse08@nctu.edu.tw>
2023-01-22 22:52:49 +0800

    finish to read the input file

--- lab2/delrepeat.cpp
+++ lab2/delrepeat.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 #include <string.h>
 
 using namespace std;
 
 int main(int argc, char **argv){
+    ifstream inputstream;
+	inputstream.open(argv[1],ios::in); 
+	
+	if(inputstream.fail()){
+		cout << "Input file is failed.";
+	}
+ 
+ 	int i=0;
+    char alpha;
+	while(inputstream >> alpha){
+		i++;
+	}
+
+ 	inputstream.close();
+    int size = i;
+    cout << size << endl;
+
     cout << "hi" << endl;
     return 0;
 }
