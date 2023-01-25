d669840d5f26ef35ae89b0f8f1c17f9b2d04ad4e
Erison88 <songerison.mse08@nctu.edu.tw>
2023-01-25 20:47:33 +0800

    add the file

---
+++ lab3/calculate_area.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string.h>
+#include "calculate_area.h"
+
+using namespace std;
+
---
+++ lab3/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string.h>
+#include <fstream>
+#include "calculate_area.h"
+
+using namespace std;
+
+int main(){
+
+    cout << "test!" << endl;
+    return 0;
+}
